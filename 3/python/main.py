674331f8d961b21968d67babfbf91575f1921d4d
teemusy <teemu.syvaoja@gmail.com>
2019-12-05 15:47:32 +0200

    Should work but numpy doesn't like  huge arrays, also realized how stupid my approach has been

--- 3/python/main.py
+++ 3/python/main.py
@@ -1,22 +1,28 @@
 import numpy as np
-from tqdm import tqdm
+from tqdm import tqdm, trange
+from time import time
+
+start_time = time()
 
 # get input data to tuples
-puzzle_input = open("../3_test.txt", "r").readlines()
+puzzle_input = open("../3_input.txt", "r").readlines()
 first_wire = tuple(puzzle_input[0].strip().split(","))
 second_wire = tuple(puzzle_input[1].strip().split(","))
 # then add them to dictionary
 wires = (first_wire, second_wire)
 
 
-class MapElement:
+"""class MapElement:
     def __init__(self, element, position=None):
         self.element = element
         self.position = position
         # number of steps
         self.steps = []
         # distance from start in manhattan
-        self.distance = None
+        self.distance = None"""
+
+# too slow to use object, moving to list
+# [name, steps[], distance]
 
 
 def find_dimensions(input_wires):
@@ -54,59 +60,52 @@ def find_dimensions(input_wires):
     return {"rows": rows, "columns": columns, "start_position": start_position}
 
 
-def build_map(input_wiremap, input_wiredata, input_wires):
+def build_map(input_wiredata, input_wires):
 
     def build(wire_number, wire_type):
-        temp_map = input_wiremap
+        temp_map = np.full((wiredata["rows"], wiredata["columns"]), "", dtype=object)
+        temp_map.fill(".")
         row = input_wiredata["start_position"][0]
         column = input_wiredata["start_position"][1]
         steps = 0
-        for element in input_wires[wire_number]:
+        print("Building wiremaps")
+        for element in tqdm(input_wires[wire_number]):
             if "R" in element:
                 value = int(element.strip("R"))
                 for _ in range(value):
                     steps += 1
                     column += 1
-                    temp_map[row][column] = MapElement(wire_type, (row, column))
-                    temp_map[row][column].steps.append(steps)
+                    temp_map[row][column] = [wire_type, steps, None]
             if "L" in element:
                 value = int(element.strip("L"))
                 for _ in range(value):
                     steps += 1
                     column -= 1
-                    temp_map[row][column] = MapElement(wire_type, (row, column))
-                    temp_map[row][column].steps.append(steps)
+                    temp_map[row][column] = [wire_type, steps, None]
             if "U" in element:
                 value = int(element.strip("U"))
                 for _ in range(value):
                     steps += 1
                     row -= 1
-                    temp_map[row][column] = MapElement(wire_type, (row, column))
-                    temp_map[row][column].steps.append(steps)
+                    temp_map[row][column] = [wire_type, steps, None]
             if "D" in element:
                 value = int(element.strip("D"))
                 for _ in range(value):
                     steps += 1
                     row += 1
-                    temp_map[row][column] = MapElement(wire_type, (row, column))
-                    temp_map[row][column].steps.append(steps)
+                    temp_map[row][column] = [wire_type, steps, None]
         return temp_map
 
     def compare(input_first, input_second):
-        output_map = np.full((wiredata["rows"], wiredata["columns"]), MapElement("."), dtype=object)
-        for i in range(len(input_first)):
+        output_map = np.full((wiredata["rows"], wiredata["columns"]), None, dtype=object)
+        output_map.fill([".", None, None])
+        output_map[input_wiredata["start_position"][0]][input_wiredata["start_position"][1]] = ["O", None, None]
+        print("Comparing wiremaps")
+        for i in trange(len(input_first)):
             for j in range(len(input_first[0])):
-                if input_first[i][j].element == input_second[i][j].element:
-                    output_map[i][j] = MapElement("+", (i, j))
-                    output_map[i][j].steps.append(input_first[i][j].steps)
-                    output_map[i][j].steps.append(input_second[i][j].steps)
-                    output_map[i][j].distance = abs(input_wiredata["start_position"][0] -
-                                                    input_wiredata["start_position"][1] +
-                                                    output_map[i][j].position[0] - output_map[i][j].position[1])
-                elif input_first[i][j].element != ".":
-                    output_map[i][j] = input_first[i][j]
-                elif input_second[i][j].element != ".":
-                    output_map[i][j] = input_second[i][j]
+                if input_first[i][j][0] == input_second[i][j][0] and input_first[i][j][0] == "+":
+                    dist = abs(input_wiredata["start_position"][0] - i) + abs(input_wiredata["start_position"][1] - j)
+                    output_map[i][j] = ["X", [input_first[i][j][1], input_second[i][j][1]], dist]
 
         return output_map
 
@@ -125,10 +124,26 @@ def build_map(input_wiremap, input_wiredata, input_wires):
 wiredata = find_dimensions(wires)
 print("Dimensions:", wiredata)
 # create 2d array where to store wiredata
-wiremap = np.full((wiredata["rows"], wiredata["columns"]), MapElement("."), dtype=object)
+wiremap = np.full((wiredata["rows"], wiredata["columns"]), "", dtype=object)
+wiremap.fill([".", None, None])
 # add starting position to map as "O"
-wiremap[wiredata["start_position"][0]][wiredata["start_position"][1]] = MapElement("O")
+wiremap[wiredata["start_position"][0]][wiredata["start_position"][1]] = ["O", None, None]
 # building wires to map
-wiremap = build_map(wiremap, wiredata, wires)
+wiremap = build_map(wiredata, wires)
 # find all crossings and their manhattan distances
-#crossing_points = np.where(wiremap.element == "X")
+manhattan_distances = []
+step_amounts = []
+for i in range(len(wiremap)):
+    for j in range(len(wiremap[0])):
+        if wiremap[i][j][0] == "X":
+            manhattan_distances.append(wiremap[i][j][2])
+            step_amounts.append((wiremap[i][j][1]))
+        #print(wiremap[i][j][0], end="")
+    #print("\n")
+
+crossing_points = np.where(wiremap[0] == "X")
+print("Closesti point:", min(manhattan_distances))
+smallest_value = min(step_amounts)
+smallest_value = smallest_value[0] + smallest_value[1]
+print("Smallest amount of steps:", smallest_value)
+print("Finished in {} seconds".format(round(time() - start_time), 3))
