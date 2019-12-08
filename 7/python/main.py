86eaf48326497b214493fbd6d823207a0ea8aaea
teemusy <teemu.syvaoja@gmail.com>
2019-12-08 19:34:54 +0200

    First part of day 7 working

--- 7/python/main.py
+++ 7/python/main.py
@@ -1,4 +1,5 @@
 from intcode import *
+from itertools import permutations
 
 
 def main():
@@ -7,15 +8,26 @@ def main():
     f.close()
     # create tuple from integers in int file
     int_code = tuple([int(n) for n in int_code.split(",")])
+    highest_value = first_part(int_code)
+    print("Highest output value:", highest_value)
 
-    phase_setting = [1,0,4,3,2]
-    error_codes = [0]
-    for i in range(5):
-        original_copy = list(int_code)
-        final_list, error_codes = process_code(original_copy, phase_setting[i], error_codes[0])
-        print(final_list)
 
-    print(error_codes)
+def first_part(int_code, phase_code=None):
+    # get all permutations of phase codes
+    phase_permutations = permutations([0, 1, 2, 3, 4])
+    # give possibility to override phase code sequence for testing
+    if phase_code:
+        phase_permutations = [phase_code]
+    highest_value = 0
+    for row in phase_permutations:
+        phase_setting = row
+        error_codes = [0]
+        for i in range(5):
+            original_copy = list(int_code)
+            final_list, error_codes = process_code(original_copy, phase_setting[i], error_codes[0])
+        if error_codes[0] > highest_value:
+            highest_value = error_codes[0]
+    return highest_value
 
 
 if __name__ == '__main__':
