06956f9b946c113bbedeb4a70d8f8303ecb4564d
teemusy <teemu.syvaoja@gmail.com>
2019-12-08 17:14:23 +0200

    Nice working solution for day 6

--- 6/python/main.py
+++ 6/python/main.py
@@ -1,41 +1,106 @@
-import numpy as np
-from string import ascii_letters
-
-
 def main():
     f = open("../6_input.txt", "r")
-    int_code = f.readlines()
+    planet_list = f.readlines()
     f.close()
     # create tuple from integers in int file
-    int_code = tuple(n.rstrip() for n in int_code)
-    # find central object
-    central_object = find_central(int_code)
-    print("Central object that the rest of the planets orbit:", central_object)
+    planet_list = [n.rstrip() for n in planet_list]
+    planet_list_copy = planet_list.copy()
+    # central object that all other objects orbit
+    central_object = "COM"
+    # insert central object in 2d list as starting point
+    celestial_objects = [[central_object]]
+    # find all objects that loop around central object and add them to next index, than do the same to them
+    index = 0
+    while len(planet_list):
+        orbiters, planet_list = find_orbiters(celestial_objects[index], planet_list)
+        celestial_objects.append(orbiters)
+        index += 1
+
+    # count orbits
+    orbits = calculate_orbits(celestial_objects)
+    print("Number of combined orbits:", orbits)
+    # find shortest route to santa
+    planet_list = planet_list_copy.copy()
+    shortest_route = find_route(planet_list)
+    print("Minimum orbital transfers required:", shortest_route)
 
 
-def find_central(int_code):
-    # divide input into 2 list, origo and orbiter
-    origo, orbiter = [], []
-    for row in int_code:
-        left, right = row.split(")")
-        origo.append(left)
-        orbiter.append(right)
+def find_route(planet_list):
+    planet_list_copy = planet_list.copy()
 
-    # find unique values in origo list
-    origo = np.array(origo)
-    origo = np.unique(origo, return_counts=True)
-    unique_origos = []
-    for i in range(len(origo[0])):
-        if origo[1][i] == 1:
-            unique_origos.append(origo[0][i])
-    origo = unique_origos
+    # find your route to start
+    starting_point = "YOU"
+    you = []
+    i = 0
+    while starting_point != "COM":
+        if starting_point in planet_list[i].split(")")[1]:
+            you.append(planet_list[i])
+            starting_point = planet_list[i].split(")")[0]
+            # remove from list to make loops faster
+            planet_list.pop(i)
+            i = 0
+        else:
+            i += 1
 
-    # find which unique value is not in the orbiter list
-    orbiter = np.array(orbiter)
-    unique = set(origo) - set(orbiter)
-    assert len(unique) == 1, "Found more than one central object!"
-    unique = str(unique).strip("{'}")
-    return unique
+    # find santas route to start
+    planet_list = planet_list_copy.copy()
+    starting_point = "SAN"
+    santa = []
+    i = 0
+    while starting_point != "COM":
+        if starting_point in planet_list[i].split(")")[1]:
+            santa.append(planet_list[i])
+            starting_point = planet_list[i].split(")")[0]
+            # remove from list to make loops faster
+            planet_list.pop(i)
+            i = 0
+        else:
+            i += 1
+
+    # split lists so only starting points are left, then reverse lists
+    you = [item.split(")")[0] for item in you]
+    santa = [item.split(")")[0] for item in santa]
+    you.reverse()
+    santa.reverse()
+
+    # loop lists and find the last common origo
+    for i in range(len(you)):
+        if you[i] != santa[i]:
+            break
+        else:
+            common_origo = you[i]
+
+    # reverse lists again and count steps until common origo
+    you.reverse()
+    santa.reverse()
+    your_steps = you.index(common_origo)
+    santas_steps = santa.index(common_origo)
+
+    step_count = your_steps + santas_steps
+
+    return step_count
+
+
+def find_orbiters(celestial_objects, planet_list):
+    orbiters = []
+    # for loop in reverse so we can remove values from it
+    for i in range(len(planet_list) - 1, -1, -1):
+        left, right = planet_list[i].split(")")
+        if left in celestial_objects:
+            orbiters.append(right)
+            planet_list.pop(i)
+    return orbiters, planet_list
+
+
+def calculate_orbits(celestial_objects):
+    orbits = 0
+    for i in range(1, len(celestial_objects)):
+        # exclude YOU and SAN
+        if "YOU" in celestial_objects[i] or "SAN" in celestial_objects[i]:
+            orbits += (len(celestial_objects[i]) - 1) * i
+        else:
+            orbits += len(celestial_objects[i]) * i
+    return orbits
 
 
 if __name__ == '__main__':
