5908a03101a429dd1c7fa10f1f72058029e2a413
teemusy <teemu.syvaoja@gmail.com>
2019-12-15 20:48:09 +0200

    First part of day 14 and testing networkx with day 15

--- 14/python/main.py
+++ 14/python/main.py
@@ -1,28 +1,93 @@
-import networkx as nx
-import matplotlib.pyplot as plt
+import re
+from time import time
+import math
+
+
+class Reaction:
+    def __init__(self, input_reaction=None, output=None):
+        self.input_reaction = input_reaction
+        self.input = []
+        self.output = output
+        if input_reaction is not None:
+            self.parse_input()
+        self.needed = 0
+
+    def parse_input(self):
+        input = self.input_reaction.split("=>")[0]
+        output = self.input_reaction.split("=>")[1]
+        if "," in input:
+            input = input.split(",")
+            for element in input:
+                amount = int(re.findall(r"[\d]+", element)[0])
+                material = re.findall(r"[a-zA-Z]+", element)
+                self.input.append((amount, material[0]))
+        else:
+            amount = int(re.findall(r"[\d]+", input)[0])
+            material = re.findall(r"[a-zA-Z]+", input)
+            self.input.append((amount, material[0]))
+        amount = int(re.findall(r"[\d]+", output)[0])
+        material = re.findall(r"[a-zA-Z]+", output)
+        self.output = (amount, material[0])
 
 
 def main():
+    start_time = time()
     f = open("../14_input.txt", "r")
     reactions = f.readlines()
     f.close()
 
     # create objects for reactions
-    reaction_list = []
-    #print(reactions)
+    reaction_list = {}
+    for row in reactions:
+        reaction_name = re.findall(r"[a-zA-Z]+", row.split("=>")[1])[0]
+        reaction_list.update({reaction_name: Reaction(row)})
+    # add fuel needed and ore object manually
+    fuel = 1
+    reaction_list["FUEL"].needed = fuel
+    reaction_list.update({"ORE": Reaction(output=(1, "ORE"))})
 
-    G = nx.Graph()
+    # find out how much we need every element
+    needed = True
+    trillion = 1000000000000
+    make_one = True
+    can_make = 0
+    index = 0
+    while needed:
+        needed = [n for n in reaction_list.values() if n.needed > 0]
+        if len(needed) == 0:
+            break
+        if needed[0].output[1] == "ORE" and len(needed) == 1:
+            break
+        for element in reaction_list.values():
+            if element.needed > 0 and element.output[1] != "ORE":
+                # how many times we need to produce reaction to create needed amount of material
+                reactions_needed = element.needed // element.output[0]
+                if reactions_needed == 0:
+                    reactions_needed = 1
+                for _ in range(reactions_needed):
+                    for input in element.input:
+                        reaction_list[input[1]].needed += input[0]
+                    element.needed -= element.output[0]
 
-    G.add_edge(1, 2, weight=4.7)
-    G.add_edges_from([(3, 4), (4, 5)], color='red')
-    G.add_edges_from([(1, 2, {'color': 'blue'}), (2, 3, {'weight': 8})])
-    G[1][2]['weight'] = 4.7
-    G.edges[3, 4]['weight'] = 4.2
 
-    nx.draw(G, with_labels=True, font_weight='bold')
-    #nx.draw_shell(G, nlist=[range(5, 10), range(5)], with_labels=True, font_weight='bold')
-    plt.show()
+    ore_needed =  reaction_list["ORE"].needed
+    if make_one:
+        print("Amount of ore needed:", ore_needed)
+    else:
+        print("Amount of fuel that can be made with 1 trillion ore:", can_make)
 
+    pattern = [[], []]
+    for element in reaction_list.values():
+        if element.needed:
+            pattern[0].append(element.output[1])
+        else:
+            pattern[1].append(element.output[1])
+    print(pattern)
+
+    if can_make == 82892753:
+        print("GREAT SUCCESS!")
+    finish_time = round(time() - start_time, 5)
+    print("Finished in time: {}\n".format(finish_time))
 
 
 if __name__ == '__main__':
--- 15/python/main.py
+++ 15/python/main.py
@@ -1,13 +1,24 @@
 from intcode import Intcode
 import numpy as np
 import pygame
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
 class Tile:
-    def __init__(self, position, tile):
+    def __init__(self, position, tile, is_start=False, adjacent=[]):
         self.position = position
         self.tile = tile
-        self.is_start = None
+        self.is_start = is_start
+        self.adjacent = adjacent
+
+    def find_adjacent(self, tiles):
+        adjacent = [n for n in tiles if any(np.all(row == self.position) for row in n.adjacent)]
+        print("Id: {}, position: {}".format(id(self), self.position))
+        self.adjacent = []
+        for element in adjacent:
+            self.adjacent.append(id(element))
+            print("Adjacent: {}, position: {}".format(id(element), element.position))
 
 
 class Robot:
@@ -23,7 +34,7 @@ def main():
 
     computer = Intcode(int_code)
     # init map, using 1d array
-    tiles = [Tile([0, 0], 1)]
+    tiles = [Tile([0, 0], 4)]
     tiles[0].is_start = True
     robot = Robot([0, 0])
 
@@ -58,6 +69,14 @@ def main():
                     pygame.draw.rect(screen, colors[element.tile], (x, y, block_x, block_y))
                 # draw robot, always in the middle of the screen
                 pygame.draw.rect(screen, colors[3], (start_x, start_y, block_x, block_y))
+
+            # calculate empty spots
+            empty_tiles = [n for n in tiles if n.tile == 1 or n.tile == 4]
+            font = pygame.font.Font('freesansbold.ttf', 15)
+            text = font.render("Number of empty tiles: {}".format(len(empty_tiles)), True, (0, 255, 0), (0, 0, 255))
+            text_rect = text.get_rect()
+            text_rect.center = (100, 20)
+            screen.blit(text, text_rect)
             pygame.display.update()
             # movement control
             key_pressed = False
@@ -70,25 +89,34 @@ def main():
                         computer.ready = True
                     if element.type == pygame.KEYDOWN:
                         if element.key == pygame.K_LEFT:
-                            print("Left")
                             computer.id_code = 3
                             key_pressed = True
                             break
                         elif element.key == pygame.K_RIGHT:
-                            print("Right")
                             computer.id_code = 4
                             key_pressed = True
                             break
                         elif element.key == pygame.K_UP:
-                            print("Up")
                             computer.id_code = 1
                             key_pressed = True
                             break
                         elif element.key == pygame.K_DOWN:
-                            print("Down")
                             computer.id_code = 2
                             key_pressed = True
                             break
+                        elif element.key == pygame.K_SPACE:
+                            G = nx.Graph()
+                            for tile in tiles:
+                                tile.find_adjacent(tiles)
+                            for tile in tiles:
+                                print(tile)
+                                print(tile.adjacent)
+                                for adjacent in tile.adjacent:
+                                    print(adjacent)
+                                    G.add_node(id(tile))
+                                    G.add_edge(id(tile), adjacent)
+                            nx.draw(G, with_labels=True, font_weight='bold')
+                            plt.show()
 
             computer.return_code = None
             while computer.return_code is None and not computer.ready:
@@ -101,17 +129,23 @@ def main():
                 2: (lambda n: n + np.array([0, 1])),
                 3: (lambda n: n + np.array([-1, 0])),
                 4: (lambda n: n + np.array([1, 0]))}
+            old_position = [robot.position[0], robot.position[1]]
             next_position = move[computer.id_code](robot.position)
             if computer.return_code != 0:
                 robot.position = move[computer.id_code](robot.position)
 
-            print("Current: {}, next: {}".format(robot.position, next_position))
             old_tile = [n for n in tiles if n.position[0] == next_position[0] and n.position[1] == next_position[1]]
-            print("Found old tile: {}, return code: {}".format(len(old_tile), computer.return_code))
             if len(old_tile) == 0 and computer.return_code != 0:
+                tiles.append(Tile(next_position, computer.return_code, adjacent=[old_position]))
+            elif len(old_tile) == 0 and computer.return_code == 0:
                 tiles.append(Tile(next_position, computer.return_code))
-            elif len(old_tile) == 0:
-                tiles.append(Tile(next_position, computer.return_code))
+            elif len(old_tile) > 0 and computer.return_code != 0:
+                # check if old position is already listed in next tile
+                found = [n for n in tiles if n.position[0] == next_position[0] and n.position[1] == next_position[1]]
+                if len(found) == 1:
+                    found2 = [n for n in found[0].adjacent if n[0] == next_position[0] and n[1] == next_position[1]]
+                    if len(found2) == 0:
+                        found[0].adjacent.append(old_position)
 
 
 if __name__ == '__main__':
