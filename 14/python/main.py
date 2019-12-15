10a7c48a056116b04909d760f7a272086dbdb54e
teemusy <teemu.syvaoja@gmail.com>
2019-12-15 21:06:30 +0200

    Trying to find pattern in day 14 part 2

--- 14/python/main.py
+++ 14/python/main.py
@@ -1,6 +1,8 @@
 import re
 from time import time
 import math
+from math import gcd
+from functools import reduce
 
 
 class Reaction:
@@ -32,7 +34,7 @@ class Reaction:
 
 def main():
     start_time = time()
-    f = open("../14_input.txt", "r")
+    f = open("../14_test.txt", "r")
     reactions = f.readlines()
     f.close()
 
@@ -43,31 +45,42 @@ def main():
         reaction_list.update({reaction_name: Reaction(row)})
     # add fuel needed and ore object manually
     fuel = 1
+
     reaction_list["FUEL"].needed = fuel
     reaction_list.update({"ORE": Reaction(output=(1, "ORE"))})
-
     # find out how much we need every element
     needed = True
     trillion = 1000000000000
     make_one = True
     can_make = 0
     index = 0
-    while needed:
-        needed = [n for n in reaction_list.values() if n.needed > 0]
-        if len(needed) == 0:
-            break
-        if needed[0].output[1] == "ORE" and len(needed) == 1:
-            break
+    pattern = []
+    while index < fuel:
+        while needed:
+            needed = [n for n in reaction_list.values() if n.needed > 0]
+            if len(needed) == 0:
+                break
+            if needed[0].output[1] == "ORE" and len(needed) == 1:
+                break
+            for element in reaction_list.values():
+                if element.needed > 0 and element.output[1] != "ORE":
+                    # how many times we need to produce reaction to create needed amount of material
+                    reactions_needed = element.needed // element.output[0]
+                    if reactions_needed == 0:
+                        reactions_needed = 1
+                    for _ in range(reactions_needed):
+                        for input in element.input:
+                            reaction_list[input[1]].needed += input[0]
+                        element.needed -= element.output[0]
+        all_zero = True
         for element in reaction_list.values():
-            if element.needed > 0 and element.output[1] != "ORE":
-                # how many times we need to produce reaction to create needed amount of material
-                reactions_needed = element.needed // element.output[0]
-                if reactions_needed == 0:
-                    reactions_needed = 1
-                for _ in range(reactions_needed):
-                    for input in element.input:
-                        reaction_list[input[1]].needed += input[0]
-                    element.needed -= element.output[0]
+            if element.needed != 0:
+                all_zero = False
+        if all_zero:
+            pattern.append(index)
+            break
+        index += 1
+
 
 
     ore_needed =  reaction_list["ORE"].needed
@@ -75,19 +88,27 @@ def main():
         print("Amount of ore needed:", ore_needed)
     else:
         print("Amount of fuel that can be made with 1 trillion ore:", can_make)
+    print(pattern)
 
-    pattern = [[], []]
+    pattern = []
     for element in reaction_list.values():
-        if element.needed:
-            pattern[0].append(element.output[1])
-        else:
-            pattern[1].append(element.output[1])
+        if element.needed and element.output[1] != "ORE":
+            pattern.append(element.needed)
     print(pattern)
 
     if can_make == 82892753:
         print("GREAT SUCCESS!")
     finish_time = round(time() - start_time, 5)
     print("Finished in time: {}\n".format(finish_time))
+
+    print("PATTERN:", find_smallest_common_denominator(pattern))
+
+    result = (trillion / -find_smallest_common_denominator(pattern)) / 43426211
+    print(result)
+
+
+def find_smallest_common_denominator(pattern):
+    return reduce(lambda a, b: a * b // gcd(a, b), pattern)
 
 
 if __name__ == '__main__':
